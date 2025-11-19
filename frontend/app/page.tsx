41a77588c9a5ea83a591ea0dec83b2be65583d52
Aryan Jain <2006aryanj@gmail.com>
2025-11-18 21:03:39 -0500

    frontend work and login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+// "use client"
+
 import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
@@ -8,51 +10,92 @@ import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 import { Suspense } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/lib/supabase/server";
+// import { useRouter } from "next/navigation";
 
 export default function Home() {
+  // const { data, error } = await supabase.auth.getSession()
+
+  const handleSubmit = async () => {
+    // const supabase = createClient();
+    // const { data, error } = await (await supabase).auth.getSession()
+    // const { session } = data
+    // const router = useRouter();
+    // if (error) throw error;
+    // if (session) {
+    //   router.push("/protected");
+    // }
+  }
+
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
-              <div className="flex items-center gap-2">
+            {/* <div className="flex gap-5 items-center font-semibold"> */}
+            {/* <div className="flex items-center gap-2">
                 <DeployButton />
-              </div>
-            </div>
-            {!hasEnvVars ? (
+              </div> */}
+            {/* </div> */}
+            {/* {!hasEnvVars ? (
               <EnvVarWarning />
-            ) : (
-              <Suspense>
-                <AuthButton />
-              </Suspense>
-            )}
+            ) : ( */}
+            <ThemeSwitcher />
+            <Suspense>
+              <AuthButton />
+            </Suspense>
+
+
+            {/* <Button onClick={handleSubmit}>
+              Start Coding
+            </Button> */}
+
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+          {/* <main className="flex-1 flex flex-col gap-6 px-4">
+            <h2 className="font-medium text-xl mb-4">Next butt steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-          </main>
+          </main> */}
+          <footer className="w-full flex flex-col items-center justify-center border-t mx-auto text-center gap-[2rem] py-[2rem]">
+            <p className="text-lg">
+              Hey everyone! I'm Aryan and I made Faang Focus to make the interview process a better place.
+              Feel free to connect with me and reach out with any suggestions or bugs :)
+            </p>
+            <div className="flex flex-row gap-[1rem]">
+              <a
+                href="https://www.linkedin.com/in/aryanjain06/"
+                target="_blank"
+                className="hover:scale-110 transition duration-550 ease-in-out"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} className="w-[3.5rem] h-[3.5rem]" />
+              </a>
+              <a
+                href="mailto:2006aryanj@gmail.com"
+                target="_blank"
+                className="hover:scale-110 transition duration-550 ease-in-out"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faEnvelope} className="w-[3.5rem] h-[3.5rem]" />
+              </a>
+              <a
+                href="https://www.instagram.com/aryan_jain_n/"
+                target="_blank"
+                className="hover:scale-110 transition duration-550 ease-in-out"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faInstagram} className="w-[3.5rem] h-[3.5rem]" />
+              </a>
+            </div>
+          </footer>
         </div>
-
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-          <ThemeSwitcher />
-        </footer>
-      </div>
-    </main>
+      </div >
+    </main >
   );
 }
