cab8ce625232c8754e9d98336b66b6db844745e5
Aryan Jain <2006aryanj@gmail.com>
2025-12-07 17:12:55 -0500

    random spacing

--- frontend/app/code/layout.tsx
+++ frontend/app/code/layout.tsx
@@ -5,12 +5,10 @@ import Navbar from "@/components/navbar";
 export default function CodeLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-
       <Navbar />
       <SidebarProvider>
         <div className="flex flex-1 overflow-hidden">
           <AppSidebar />
-
           <main className="flex-1 overflow-y-auto">
             {children}
           </main>
